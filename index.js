379f3f0c1adf03f60dbcf061023e878751c71a2a
Terry Blue <terryblue@comcast.net>
2018-03-02 23:23:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 function searchRepositories() {
   console.log("*** searchRepositories()")
-  const req = new XMLHttpRequest()
+  const req
   var searchTerms = document.getElementById("searchTerms").value
   // req.addEventListener("load", showRepositories);
   var url = `https://api.github.com/search/repositories?q=${searchTerms}`

ced64f9def34d274592068761d4859829be7ea54
Terry Blue <terryblue@comcast.net>
2018-03-04 23:25:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
-var displayError = () => $('#errors').html("I'm sorry, there's been an error. Please try again.")
-
-var renderCommit = (commit) => {
-  return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
-}
-
-var renderCommits = (data) => {
-  let result = data.map((commit)=>renderCommit(commit)).join('')
-  return `<ul>${result}</ul>`
-}
-
-var showCommits = (el) => {
-  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, data => {
-    $('#details').html(renderCommits(data))
-  }).fail(error => {
-    displayError()
-  })
-}
-
-var renderSearchResult = (result) => {
-  return `
-      <div>
-        <h2><a href="${result.html_url}">${result.name}</a></h2>
-        <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
-        <p>${result.description}</p>
-      </div>
-      <hr>
-    `
-}
-
-var renderSearchResults = (data) => data.items.map( result => renderSearchResult(result))
-
-var searchRepositories = () => {
-  const searchTerms = $('#searchTerms').val()
-  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
-      $('#results').html(renderSearchResults(data))
-    }).fail(error => {
-      displayError()
-    })
-}
-
-$(document).ready(function (){
+// Register a partial with Handlebars when the page loads
+document.addEventListener("DOMContentLoaded", function(event) {
+  Handlebars.registerPartial("authorPartial", document.getElementById("author-partial-template").innerHTML)
 });
+
+function searchRepositories() {
+  // const req = new XMLHttpRequest()
+  var searchTerm = document.getElementById("searchTerms").value
+  var url = `https://api.github.com/search/repositories?q=${searchTerm}`
+  $(document).ready(function (){
+    $.get(url, function(data) {
+      // This is executed when the url file request succeeds
+      const src = document.getElementById("repository-template").innerHTML
+      const template = Handlebars.compile(src)
+      const repoList = template(data.items)
+      document.getElementById("results").innerHTML = repoList
+    }).fail(function() {
+      // This is called when an error occurs
+      displayError()
+    });
+  });
+}
+
+function showCommits(userName, repo) {
+  const details = "Commits"
+  var url = `https://api.github.com/repos/${userName}/${repo}/commits`
+  $(document).ready(function (){
+    $.get(url, function(data) {
+      // This is executed when the .html file request exists
+      const src = document.getElementById("commits-template").innerHTML
+      const template = Handlebars.compile(src)
+      const commitList = template(data)
+      $("#details").empty().append(details + " for Repository: " + repo + commitList);
+    }).fail(function() {
+      // This is called when an error occurs
+      displayError()
+    });
+  });
+}
+
+function displayError() {
+  var errorMessage = "I'm sorry, there's been an error. Please try again."
+  document.getElementById("errors").innerHTML = errorMessage
+}
